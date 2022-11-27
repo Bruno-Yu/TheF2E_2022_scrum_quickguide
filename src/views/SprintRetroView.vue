5e01bb64a356f9995a05c66f81c65b4efa0d3830
Bruno Yu <jackhellowin@gmail.com>
2022-11-27 20:55:39 +0800

    加入頁簽 logo 以及 SpringRetroView

--- src/views/SprintRetroView.vue
+++ src/views/SprintRetroView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-[72px]">
-    <div class="flex items-start mt-6 justify-center mb-8">
+    <div class="flex items-start mt-6 justify-center mb-8 mx-auto">
       <div class="mr-[42px] w-[180px] shrink-0">
         <img
           class="block h-auto"
@@ -29,6 +29,68 @@
         </div>
       </div>
     </div>
+    <div class="grid grid-cols-2 gap-5 mx-auto">
+      <div class="flex flex-col">
+        <h3 class="text-center text-h2 text-yellow mb-6">做得好的地方</h3>
+        <ul class="list-none w-full">
+          <li class="w-full" v-for="(item, index) in GoodArr" :key="index">
+            <button
+              type="button"
+              class="w-full bg-bg10 p-6 text-h3 text-white flex items-center min-h-[152px]"
+              :class="{ active: chosenGood === index ? true : false }"
+              @click="selectGood(item, index)"
+            >
+              <div class="w-[40px] h-[40px] shrink-0 mr-4">
+                <img
+                  v-if="chosenGood !== index"
+                  src="@/assets/images/Union.png"
+                  alt="Union"
+                />
+                <img
+                  v-else
+                  src="@/assets/images/Active_union.png"
+                  alt="Active_union"
+                />
+              </div>
+              <p class="whitespace-wrap text-left">
+                {{ item.content }}
+              </p>
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div class="flex flex-col">
+        <h3 class="text-center text-h2 text-yellow mb-6">
+          有哪些可以做得更好？
+        </h3>
+        <ul class="list-none w-full">
+          <li class="w-full" v-for="(item, index) in BetterArr" :key="index">
+            <button
+              type="button"
+              class="w-full bg-bg10 p-6 text-h3 text-white flex items-center min-h-[152px]"
+              :class="{ active: chosenBetter === index ? true : false }"
+              @click="selectBetter(item, index)"
+            >
+              <div class="w-[40px] h-[40px] shrink-0 mr-4">
+                <img
+                  v-if="chosenBetter !== index"
+                  src="@/assets/images/Union.png"
+                  alt="Union"
+                />
+                <img
+                  v-else
+                  src="@/assets/images/Active_union.png"
+                  alt="Active_union"
+                />
+              </div>
+              <p class="whitespace-wrap text-left">
+                {{ item.content }}
+              </p>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
     <!-- button -->
     <div class="absolute right-[80px] bottom-[80px] flex">
       <button
@@ -38,16 +100,13 @@
       >
         <p class="relative z-10">&lt;</p>
       </button>
-      <button
-        type="button"
-        class="btn-base"
-        @click="$router.push({ path: '/final' })"
-      >
+      <button type="button" class="btn-base" @click="checkAws">
         <p class="relative z-10">我想我瞭解了!</p>
       </button>
     </div>
     <!-- fail -->
     <div
+      ref="failure"
       class="hidden absolute bg-bg0/80 inset-0 flex justify-center items-center z-10"
     >
       <div class="flex flex-col items-center">
@@ -63,7 +122,7 @@
         <button
           type="button"
           class="btn-base w-[180px]"
-          @click="$router.push({ path: '/product' })"
+          @click="$refs.failure.classList.add('hidden')"
         >
           <p class="relative z-20">再試試看</p>
         </button>
@@ -71,6 +130,7 @@
     </div>
     <!-- success -->
     <div
+      ref="success"
       class="hidden absolute bg-bg0/80 inset-0 flex justify-center items-center z-10"
     >
       <div class="flex flex-col items-center">
@@ -86,7 +146,7 @@
         <button
           type="button"
           class="btn-base w-[250px]"
-          @click="$router.push({ path: '/product' })"
+          @click="$router.push({ path: '/final' })"
         >
           <p class="relative z-20">完成挑戰</p>
         </button>
@@ -101,9 +161,60 @@ import { useStore } from "@/stores/useStore";
 
 export default {
   computed: { ...mapWritableState(useStore, ["currentPage", "show"]) },
+  data() {
+    return {
+      GoodAws: false,
+      BetterAws: false,
+      chosenGood: null,
+      chosenBetter: null,
+      GoodArr: [
+        {
+          content: "這次我幫了很多人救火耶。",
+          result: false,
+        },
+        {
+          content: "大家在開發上都會互相 cover，讓任務準時在時間內完成。",
+          result: true,
+        },
+      ],
+      BetterArr: [
+        {
+          content: "可以記錄這次的開發時間，讓預估團隊點數可以更精準。",
+          result: true,
+        },
+        {
+          content: "開發時間預估不準確，請後端下次改進，避免 delay 到我。",
+          result: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    selectGood(item, index) {
+      this.GoodAws = item.result;
+      this.chosenGood = index;
+    },
+    selectBetter(item, index) {
+      this.BetterAws = item.result;
+      this.chosenBetter = index;
+    },
+    checkAws() {
+      if (this.BetterAws && this.GoodAws) {
+        this.$refs.success.classList.remove("hidden");
+      } else {
+        this.$refs.failure.classList.remove("hidden");
+      }
+    },
+  },
   mounted() {
     this.show = true;
     this.currentPage = 9;
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.active {
+  @apply text-yellow;
+}
+</style>
